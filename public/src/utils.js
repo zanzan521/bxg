59b5d42e0b996997c0b739f5deca13c97ad1513a
zxj <839638337@qq.com>
2017-04-19 20:28:04 +0800

    对导航选中状态做了优化

--- public/src/utils.js
+++ public/src/utils.js
@@ -1,7 +1,7 @@
 define(['jquery'],function(){
 	return{
 		setMenu:function(key){
-		$('.navs a[href="'+ key +'"]').addClass('active');
+		$('.navs a[href="'+ key +'"]').addClass('active').parents('ul').show();
 	}
 	}
 })
